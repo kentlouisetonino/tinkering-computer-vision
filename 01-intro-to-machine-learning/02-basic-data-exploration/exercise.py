d6844366f1e0ddfc129c10cb18009b706ebb933c
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-02-26 21:52:28 +0800

    Problem 1 solution.

--- 01-intro-to-machine-learning/02-basic-data-exploration/exercise.py
+++ 01-intro-to-machine-learning/02-basic-data-exploration/exercise.py
@@ -1,19 +1,26 @@
-# Pandas is a library use for exploring and manipulating data.
-# DataFrame holds the type of data you might think of as a table.
-
 import pandas as pd
+from datetime import datetime
 
-# File path variable.
-melbourne_file_path = './data_melbourne_housing.csv'
+# Melbourne housing data file path.
+melb_file = './data_melbourne_housing.csv'
 
-# Read the data and store data in DataFrame.
-melbourne_df = pd.read_csv(melbourne_file_path)
+# Read the melbourne housing data and store the dataframe.
+melb_df = pd.read_csv(melb_file)
 
-# Melbourne data sections.
-melbourne_summary = melbourne_df.describe()
-melbourne_columns_list = melbourne_df.columns
+# Melbourne summary.
+melb_summary = melb_df.describe()
 
-# Print the sections.
-print("**DATA SUMMARY**", "\n", melbourne_summary)
-print("\n")
-print("**DATA COLUMNS LIST**", "\n", melbourne_columns_list)
+# Melbourne columns list.
+melb_columns_list = melb_df.columns
+
+# Melbourne rows list.
+melb_rows_list = melb_df
+
+# Getting the melbourne maximum year built.
+melb_year_built = melb_df["YearBuilt"]
+melb_max_year_built_value = melb_df.loc[melb_year_built.idxmax(), 'YearBuilt']
+
+# Problem 2: As of today, how old is the newest home.
+current_year = datetime.now().year
+newest_home_age = current_year - melb_max_year_built_value
+print("NEWEST HOME AGE", "\n", newest_home_age)
