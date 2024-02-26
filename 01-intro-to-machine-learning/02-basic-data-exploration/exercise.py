8ed0892ce02fc2a09890426feaf44dd487ea993f
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-02-26 22:07:26 +0800

    Solve the exercise 1 problem.

--- 01-intro-to-machine-learning/02-basic-data-exploration/exercise.py
+++ 01-intro-to-machine-learning/02-basic-data-exploration/exercise.py
@@ -16,11 +16,16 @@ melb_columns_list = melb_df.columns
 # Melbourne rows list.
 melb_rows_list = melb_df
 
-# Getting the melbourne maximum year built.
-melb_year_built = melb_df["YearBuilt"]
-melb_max_year_built_value = melb_df.loc[melb_year_built.idxmax(), 'YearBuilt']
+# Problem 1: What is the average lot size.
+melb_building_area = melb_df["BuildingArea"]
+melb_building_area_mean = melb_building_area.mean()
+print("AVERAGE LOT SIZE", "\n", round(melb_building_area_mean))
+print("\n")
 
 # Problem 2: As of today, how old is the newest home.
+melb_year_built = melb_df["YearBuilt"]
+melb_max_year_built_value = melb_df.loc[melb_year_built.idxmax(), 'YearBuilt']
 current_year = datetime.now().year
 newest_home_age = current_year - melb_max_year_built_value
-print("NEWEST HOME AGE", "\n", newest_home_age)
+print("NEWEST HOME AGE", "\n", round(newest_home_age))
+print("\n")
