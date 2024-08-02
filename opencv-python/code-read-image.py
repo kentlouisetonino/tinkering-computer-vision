056ca3510a1715f7039c7c09f64cdc78a9862181
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-08-02 21:33:43 +0800

    Changes.

--- opencv-python/code-read-image.py
+++ opencv-python/code-read-image.py
@@ -2,15 +2,15 @@ import cv2
 import sys
 
 # Read the image.
+# If you print the img, it return a matrix of pixels with values from 0 to 255.
 img = cv2.imread(cv2.samples.findFile("images/drone.jpg"))
 
-print(img)
-
+# Check if the file exist.
 if img is None:
     sys.exit("Could not read the image.")
 
-cv2.imshow("Display window", img)
-k = cv2.waitKey(0)
+# Display the image.
+cv2.imshow("Drone Image", img)
 
-if k == ord("s"):
-    cv2.imwrite("drone.jpg", img)
+# Wait for the user to enter a key.
+cv2.waitKey(0)
