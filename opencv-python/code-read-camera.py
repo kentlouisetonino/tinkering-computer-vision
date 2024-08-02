c0aa0f1fc32da5ebaab61f1a0a22f5b2e3588130
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-08-02 22:06:53 +0800

    video.

---
+++ opencv-python/code-read-camera.py
@@ -0,0 +1,30 @@
+import cv2
+
+# Capture the video using a camera.
+capture = cv2.VideoCapture(0)
+
+# Check if camera is open.
+if not capture.isOpened():
+    print("Cannot open camera")
+    exit()
+
+# Capture the camera frame by frame.
+while True:
+    ret, frame = capture.read()
+
+    # If return is false.
+    if not ret:
+       print("Cannot receive frame.")
+       break
+
+    # Handle the frame.
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame.
+    cv2.imshow("frame", gray)
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
