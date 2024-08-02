692f0a641e0a4c9bcda9d3234a7c193136b714f7
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-08-02 20:29:42 +0800

    Unnecessary changes.

--- tinkering-opencv-cpp/code-read-image.cpp
+++ tinkering-opencv-cpp/code-read-image.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+using namespace cv;
+using namespace std;
 
-int main(void) {
-  std::cout << "Hello World" << std::endl;
-
+int main() {
+  std::string image_path = samples::findFile("test.jpg");
   return 0;
 }
