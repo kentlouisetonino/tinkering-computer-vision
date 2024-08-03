8ac447990b991926e2d7b1639bf1d05f49d56c3d
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-08-03 14:26:31 +0800

    Playing around drawing a line.

--- opencv-python/01-gui-features/drawing-line.py
+++ opencv-python/01-gui-features/drawing-line.py
@@ -4,8 +4,15 @@ import cv2
 # Create a black image.
 image = numpy.zeros((500, 500, 3), numpy.uint8)
 
-# Draw a diagonal blue line with thickness of 5 px.
-cv2.line(image, (10, 10), (490, 490), (255, 0, 0), 2)
+# Draw a blue square using lines with thickness of 5 px.
+cv2.line(image, (10, 10), (490, 10), (255, 0, 0), 2)
+cv2.line(image, (490, 10), (490, 490), (255, 0, 0), 2)
+cv2.line(image, (490, 490), (10, 490), (255, 0, 0), 2)
+cv2.line(image, (10, 490), (10, 10), (255, 0, 0), 2)
+
+# Add a two red diagonal line.
+cv2.line(image, (10, 10), (490, 490), (0, 0, 255), 2)
+cv2.line(image, (10, 490), (490, 10), (0, 0, 255), 2)
 
 # Show the image.
 cv2.imshow("Drawing Line", image);
